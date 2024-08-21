2aff2ea24a4e10ca6bd402f84fba8cf03c990e0c
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2024-08-21 18:02:40 +0200

    Create home.py

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+# read text from index.txt
+with open('index.html', 'r') as file:
+    html_content = file.read()
+
+html(html_content, height=600)
