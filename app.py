1f67d363c05165c0d1c53f1091ff646493fb7085
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2024-08-21 13:12:12 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+pages = [
+    st.Page("home.py", title="Home"),
+    st.Page("sql_mystery_game.py", title="Play"),
+    st.Page("leaderboard.py", title="Leaderboard"),
+    st.Page("info.py", title="About Project")
+]
+
+pg = st.navigation(pages, position='hidden')
+pg.run()
