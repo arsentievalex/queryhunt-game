9be64831c3136304a409c07ace60569a01194e2c
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2024-08-24 17:18:22 +0200

    Update workflow.py

--- utils/workflow.py
+++ utils/workflow.py
@@ -127,7 +127,7 @@ class ValidatedSqlEvent(Event):
 class MysteryFlow(Workflow):
 
     # get unique user token from streamlit headers
-    web_socket_key = st.context.headers["Sec-Websocket-Key"]
+    web_socket_key = st.context.headers["X-Streamlit-User"]
     user_token = re.sub(r'[^A-Za-z0-9]', '', web_socket_key)
 
     try:
